7be55f27ddf2b7234b2f7484da82f90fb7bca49a
Thomaszz4 <Qiming.Huang19@student.xjtlu.edu.cn>
2020-12-22 23:10:06 +0800

    update for flask

--- website.py
+++ website.py
@@ -1,21 +1,55 @@
 from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def access():
     return render_template('check.html')
 
 @app.route('/check', methods=['POST'])
 def check():
     if request.form.get('password') == 'zhuzhu520':
         return render_template('index.html')
+    else:
+        return render_template('check_wrong.html')
 
 @app.route('/index.html')
-def add():
+def index():
     return render_template('index.html')
+
+@app.route('/main.html')
+def main():
+    return render_template('main.html')
 
 @app.route('/add_successfully', methods=['POST'])
 def add_comment():
-    with open('/Users/huangqiming/Desktop/web/comments.txt', 'w') as fr:
+    with open('/Users/huangqiming/Desktop/html/Thomaszz4.github.io/comments.txt', 'w') as fr:
         fr.write(request.form.get('message'))
     return "Successfully add comments"
+
+@app.route('/comment.html')
+def comment():
+    return render_template('comment.html')
+
+@app.route('/work.html')
+def work():
+    return render_template('work.html')
+
+@app.route('/reference.html')
+def reference():
+    return render_template('reference.html')
+
+@app.route('/note.html')
+def note_():
+    return render_template('note.html')
+
+@app.route('/log.html')
+def log():
+    return render_template('log.html')
+
+@app.route('/errors.html')
+def errors():
+    return render_template('errors.html')
+
+if __name__ == '__main__':
+    app.run()
